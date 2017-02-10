43c77edb641c5e878ba258a3cfd37a355e38c204
lol838337 <lol838337@gmail.com>
2017-02-11 00:25:49 +0530

    (untested) added install for windows

---
+++ install-windows/install.bat
@@ -0,0 +1,5 @@
+start ./python-3.6.0.exe
+start python -m pip install -U pip
+start pip install virtualenvwrapper-win
+mkvirtualenv ../venv-win
+cmd pip install django
---
+++ install-windows/run.bat
@@ -0,0 +1,4 @@
+start workon ../venv-win
+cd ../slider
+start python3 manage.py runserver
+start http://127.0.0.1/offline/album/
