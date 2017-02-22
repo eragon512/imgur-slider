9c4c05c40af0248f577848f1e236b571a8c88a67
lol838337 <lol838337@gmail.com>
2017-02-23 00:46:30 +0530

    slider/urls.py: removed ref to 'online' app

--- slider/slider/urls.py
+++ slider/slider/urls.py
@@ -20,6 +20,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^offline/', include('offline_storage.urls')),
-    url(r'^online/', include('online.urls')),
     url(r'^admin/', admin.site.urls),
 ]
