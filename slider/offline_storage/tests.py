76f8cbd966b1ec22543637886af3d0b748195223
lol838337 <lol838337@gmail.com>
2017-02-23 19:01:14 +0530

    tests: added tests to check bare functionality

--- slider/offline_storage/tests.py
+++ slider/offline_storage/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
+
+from offline_storage.models import *
+from offline_storage.views import *
 
 # Create your tests here.
+class AlbumDownloadTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_server(self):
+        response = self.client.get('/offline/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_album_storage(self):
+        request = self.client.get('/offline/album/')
+        response = album(request,'pyyvA')
+        self.assertEqual(response.status_code, 200)
+    # still not working right
+    #def test_form_process(self):
+        #request = self.client.post('/offline/album/',{"album_url": "http://imgur.com/a/MUsSI"})
+        #response = album_list(request)
+        #response.client = self.client
+        #self.assertRedirects(response, reverse("/offline/album/"))
+        #self.assertEqual(redirect.status_code, 200)
